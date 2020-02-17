30e749158df99c1d6ed59e5dae4b3c8b224755f6
Harika <harika.k.233@gmail.com>
2020-02-17 06:49:41 +0000

    Js Calculator done

--- js-assignments/assignment-4/calculator.js
+++ js-assignments/assignment-4/calculator.js
@@ -1,18 +1,30 @@
 //Write your javascript logic here
+let user_input = '';
+let plus_symbol = (value) => {
+  user_input = user_input + value;
+  console.log(user_input);
+  document.getElementById('output').innerHTML = user_input;
+}
 
-function getExpressionOutput(input) {
+let ac_symbol = (value) => {
+  user_input = '';
+  document.getElementById('output').innerHTML = user_input;
+}
+
+function getExpressionOutput(user_input) {
   try {
-    return eval(input)
+    return eval(user_input);
   }
   catch (e) {
-    console.warn('Invalid expression', input)
-    return input
+    user_input = "Invalid Expression";
+    return user_input
   }
 }
 
 function onClickShowOutput() {
-  let expression = '5+2*10'
-  const output = getExpressionOutput(expression)
-  console.log(output)
-  document.getElementById('output').innerHTML = output
+  let expression = user_input;
+  const output = getExpressionOutput(expression);
+  console.log(output);
+  document.getElementById('output').innerHTML = output;
+  user_input = output;
 }
